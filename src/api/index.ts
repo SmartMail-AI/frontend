fe80963087bb2a4f9bd9c2859588a2851ab4dbed
cla6shade <cla6shade@gmail.com>
2025-06-02 00:55:29 +0900

    feat: fetch method 정의

---
+++ src/api/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { getStoredValue } from '../utils/storage';
+import type { DetailedEmail, GetEmailsResponse } from '../types';
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL! + '/api',
+  headers: {
+    authorization: `Bearer ${getStoredValue('token') || ''}`,
+  }
+});
+
+export async function fetchEmails({ pageParam: pageToken }: { pageParam?: string }) {
+  const response = await axiosInstance.get<GetEmailsResponse>('/emails', {
+    params: {
+      page_token: pageToken,
+      max_results: 10,
+    }
+  });
+  return response.data;
+}
+
+export async function fetchEmail(id?: string) {
+  if(! id)
+    return;
+  const response = await axiosInstance.get<DetailedEmail>(`/emails/${id}`);
+  return response.data;
+}
