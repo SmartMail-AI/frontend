9141f87a38476b5bd40fa6b1dad7dbe3f0e88471
cla6shade <cla6shade@gmail.com>
2025-06-02 00:56:44 +0900

    refactor: 로그인 컨텍스트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { LoginContextProvider } from './contexts/LoginContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LoginContextProvider>
+      <App />
+    </LoginContextProvider>
   </StrictMode>,
 )
