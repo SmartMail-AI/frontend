87d688b130a550e3b5f90563099ceaf0dae8815b
cla6shade <cla6shade@github.com>
2025-06-04 00:29:51 +0900

    chore: dev 서버 실행 이전에 mock worker setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { LoginContextProvider } from './contexts/LoginContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <LoginContextProvider>
-      <App />
-    </LoginContextProvider>
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'development' && !import.meta.env.VITE_MSW_ENABLED) {
+    return;
+  }
+
+  const { worker } = await import('./mocks');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  console.log('Mock service worker has been initialized');
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <LoginContextProvider>
+          <App />
+        </LoginContextProvider>
+      </QueryClientProvider>
+    </StrictMode>,
+  )
+});
