bbdb4b5c5f5d393ac398c2bda9bc3ca26e47551f
cla6shade <cla6shade@github.com>
2025-06-05 13:56:16 +0900

    feat: category 필터링 기능 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { getStoredValue } from '../utils/storage';
-import type { DetailedEmail, GetEmailsResponse } from '../types';
+import type {
+  DetailedEmail,
+  GetAuthUrlResponse,
+  GetEmailsResponse,
+} from '../types';
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL! + '/api',
@@ -9,13 +13,21 @@ export const axiosInstance = axios.create({
   }
 });
 
-export async function fetchEmails({ pageParam: pageToken }: { pageParam?: string }) {
+export async function fetchAuthUrl() {
+  const response = await axiosInstance.get<GetAuthUrlResponse>('/auth/google');
+  return response.data;
+}
+
+export async function fetchEmails({ pageParam: pageToken, selectedCategory }
+                                    : { pageParam?: string, selectedCategory?: string }) {
   const response = await axiosInstance.get<GetEmailsResponse>('/emails', {
     params: {
       page_token: pageToken,
       max_results: 10,
+      category: selectedCategory,
     }
   });
+
   return response.data;
 }
 
@@ -25,6 +37,6 @@ export async function fetchEmail(id: string) {
 }
 
 export async function fetchCategories() {
-  const response = await axiosInstance.get<string[]>('/categories');
+  const response = await axiosInstance.get<string[]>('/emails/categories');
   return response.data;
 }
