8e57f108719ca67f1d63e278443d7d04a1a90baf
cla6shade <cla6shade@github.com>
2025-06-05 13:57:09 +0900

    feat: 로그인/로그아웃 구현 및 suspense fallback 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import { Suspense, use, useEffect, useState } from 'react';
+import { Suspense, use, useCallback, useState } from 'react';
 import { ScrollArea } from './components/ui/scroll-area';
 import CategoryListView from './features/CategoryListView';
 import SelectedEmailView from './features/SelectedEmailView';
 import PaginatedEmailView from './features/PaginatedEmailView';
 import { useQuery } from '@tanstack/react-query';
-import { fetchCategories } from './api';
+import { fetchAuthUrl, fetchCategories } from './api';
 import LoginContext from './contexts/LoginContext';
-import { useNavigate } from 'react-router';
+import CenteredSpinner from './components/centered-spinner';
+import { Button } from './components/ui/button';
+import { invalidateToken } from './utils/storage';
 
 export default function App() {
   const [selectedCategory, setSelectedCategory] = useState<string>();
   const [selectedEmailId, setSelectedEmailId] = useState<string>();
+  const [currentPage, setCurrentPage] = useState(1); //page index
   const { isLoggedIn } = use(LoginContext);
   const { data: categoryData, isFetching: isCategoryFetching } = useQuery({
     queryKey: ['categories'],
     queryFn: fetchCategories,
+    initialData: [],
   });
-  const navigate = useNavigate();
-  
-  useEffect(() => {
-    if(! isLoggedIn) {
-      navigate('/login');
-    }
-  }, [navigate, isLoggedIn]);
+
+  const handleLoginClick = useCallback(async() => {
+    const { authorization_url: url } = await fetchAuthUrl();
+    window.location.href = url;
+  }, []);
+
+  const handleLogoutClick = useCallback(async() => {
+    invalidateToken();
+    window.location.reload();
+  }, []);
 
   return (
     <div className="flex h-screen bg-background">
@@ -31,49 +38,72 @@ export default function App() {
         <div className="p-4">
           <h2 className="text-lg font-semibold mb-4">이메일 분류</h2>
           <ScrollArea className="h-[calc(100vh-120px)]">
-            <div className="space-y-1">
-              {
-                isCategoryFetching ? null : (
-                  <CategoryListView
-                    categories={categoryData!}
-                    selectedCategory={selectedCategory}
-                    setSelectedCategory={setSelectedCategory}
-                  />
-                )
-              }
-            </div>
+            {
+              isLoggedIn ?
+                <div className='space-y-1'>
+                  {
+                    isCategoryFetching ? <CenteredSpinner /> : (
+                      <CategoryListView
+                        categories={categoryData!}
+                        selectedCategory={selectedCategory}
+                        setSelectedCategory={(category) => {
+                          setSelectedCategory(category);
+                          setCurrentPage(1);
+                        }}
+                      />
+                    )
+                  }
+                </div> : null
+            }
           </ScrollArea>
         </div>
       </div>
 
-      {/* AI 카테고리 또는 이메일 목록 */}
-      <div className={selectedEmailId ? "w-96 border-r" : "flex-1"}>
-        <div className="p-4 border-b">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <h3 className="font-medium">{selectedCategory}</h3>
+      {/* 이메일 목록 */}
+      <div className={`flex flex-col ${selectedEmailId ? 'w-96 border-r' : 'flex-1'}`}>
+        <div className='p-4 border-b'>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center gap-2'>
+              <h3 className='font-medium'>{selectedCategory || '전체'}</h3>
             </div>
-            <span className="text-sm text-muted-foreground">10개의 이메일</span>
+            {
+              isLoggedIn ?
+                <Button onClick={handleLogoutClick}>로그아웃</Button> :
+                <Button onClick={handleLoginClick}>로그인</Button>
+            }
           </div>
         </div>
-        <Suspense>
-          <PaginatedEmailView
-            setSelectedEmailId={setSelectedEmailId}
-            selectedEmailId={selectedEmailId}
-            selectedCategory={selectedCategory}
-          />
-        </Suspense>
+        {
+          isLoggedIn ?
+            <Suspense fallback={<CenteredSpinner />}>
+              <PaginatedEmailView
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                setSelectedEmailId={setSelectedEmailId}
+                selectedEmailId={selectedEmailId}
+                selectedCategory={selectedCategory}
+              />
+            </Suspense>
+            :
+            <div className='flex items-center justify-center flex-1'>
+              <p>
+                로그인하여 계속하세요.
+              </p>
+            </div>
+        }
       </div>
       {
-        selectedEmailId && (
-          <Suspense fallback={<p>Loading..</p>}>
-            <SelectedEmailView
-              setSelectedEmailId={setSelectedEmailId}
-              selectedEmailId={selectedEmailId}
-            />
-          </Suspense>
+        (selectedEmailId && isLoggedIn) && (
+          <div className='flex-1 h-screen'>
+            <Suspense fallback={<CenteredSpinner />}>
+              <SelectedEmailView
+                setSelectedEmailId={setSelectedEmailId}
+                selectedEmailId={selectedEmailId}
+              />
+            </Suspense>
+          </div>
         )
       }
-    </div>
+  </div>
   )
 }
