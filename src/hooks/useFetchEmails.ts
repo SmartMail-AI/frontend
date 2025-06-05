efd17dfa6494bef4c71c98bd5a9de798d140056e
cla6shade <cla6shade@github.com>
2025-06-05 13:53:30 +0900

    feat: 이전 페이지로 돌아가기 기능 구현

--- src/hooks/useFetchEmails.ts
+++ src/hooks/useFetchEmails.ts
@@ -1,9 +1,15 @@
 import {
+  type InfiniteData,
   useSuspenseInfiniteQuery,
 } from '@tanstack/react-query';
 import { fetchEmails } from '../api';
+import type { AxiosError } from 'axios';
 
-export default function useFetchEmails() { // 추후 category 추가
+interface UseFetchEmailsProps {
+  selectedCategory?: string;
+}
+
+export default function useFetchEmails({ selectedCategory }: UseFetchEmailsProps) { // 추후 category 추가
   const {
     fetchNextPage: fetchNextEmailPage,
     isFetchingNextPage: isFetchingNextEmailPage,
@@ -12,11 +18,18 @@ export default function useFetchEmails() { // 추후 category 추가
     data: emailPageData,
     hasNextPage: hasNextEmailPage,
     hasPreviousPage: hasPreviousEmailPage,
-  } = useSuspenseInfiniteQuery({
-    queryKey: ['emails'],
-    queryFn: fetchEmails,
+  } = useSuspenseInfiniteQuery<
+    Awaited<ReturnType<typeof fetchEmails>>,
+    AxiosError,
+    InfiniteData<Awaited<ReturnType<typeof fetchEmails>>>,
+    [string, string?],
+    string | undefined
+  >({
+    queryKey: ['emails', selectedCategory],
+    queryFn: ({ pageParam }) => fetchEmails({ pageParam, selectedCategory }),
     initialPageParam: undefined,
     getNextPageParam: (lastPage) => lastPage.next_page_token,
+    getPreviousPageParam: (firstPage) => firstPage.previous_page_token,
   });
 
   return {
