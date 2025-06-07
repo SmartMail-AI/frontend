23bc3d3b1f0b2470b4024b952a47de56ecc5cc45
cla6shade <cla6shade@gmail.com>
2025-06-07 17:42:54 +0900

    fix: 리버스 프록시 관련 문제 해결

--- backend/main.py
+++ backend/main.py
@@ -39,4 +39,4 @@ setup_email_polling(app)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, proxy_headers=True)
